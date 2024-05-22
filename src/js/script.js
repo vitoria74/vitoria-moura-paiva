dd25bfa86eab3679f1de85bfed4d73fabd198390
vitoria74 <maria_vitoria74@hotmail.com>
2024-05-22 13:04:34 -0300

    criação de js + variáveis

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// DOM = representação do html
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)'); // querySelectorAll vai selecionar todos os projetos inativos. All pq é mais de um
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    
+}); // add uma escuta ao click do botão e o que vai acontecer quando receber click
