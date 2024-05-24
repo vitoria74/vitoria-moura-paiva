d8e7e07b760f105592285b9fc1195bb494b3e7d9
vitoria74 <maria_vitoria74@hotmail.com>
2024-05-24 00:30:57 -0300

    comentários excluídos

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,17 @@
 // DOM = representação do html
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)'); // querySelectorAll vai selecionar todos os projetos inativos. All pq é mais de um
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)'); 
 
 botaoMostrarProjetos.addEventListener('click', () => {
-    //Add a classe 'ativo' nos projetos escondidos
     mostrarMaisProjetos();
+    esconderBotao(); 
+}); 
 
-    //esconder o botão 'mostrar mais'
-    esconderBotao(); // add a classe remover no bootão pra sumir
-}); // add uma escuta ao click do botão e o que vai acontecer quando receber click
-
-function esconderBotao() { //segundo código refatorado
+function esconderBotao() { 
     botaoMostrarProjetos.classList.add('remover');
 }
 
-function mostrarMaisProjetos() { //código refatorado
+function mostrarMaisProjetos() {
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
     });
